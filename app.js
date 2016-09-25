bfd6541ac2b0b15daa27fff5b6ce92f2df74a553
Daoyun Zeng <daoyun.zeng@gmail.com>
2016-09-24 19:55:17 -0700

    expand limitation of the request.

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var api = require('./server/api')(io, config);
 
 app.use(logger('dev'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 
